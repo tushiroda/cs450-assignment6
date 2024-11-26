31848a8a0c6fc608cce6749fed5c1e51f41778d1
TaeshinUshiroda <taeshinushiroda@gmail.com>
2024-11-26 15:06:24 -0500

    setup fileupload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FileUpload from "./FileUpload";
+import Streamgraph from "./Streamgraph";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  set_data = (csv_data) => {
+    this.setState({ data: csv_data });
+  };
+
+  render() {
+    return (
+      <div>
+        <FileUpload set_data={this.set_data}></FileUpload>
+        <div className="parent">
+          <Streamgraph csv_data={this.state.data}></Streamgraph>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
